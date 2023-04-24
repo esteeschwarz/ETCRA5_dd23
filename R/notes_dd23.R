7053e9fc3965814e695d7d6454ae156431ac07ae
estee <91368979+esteeschwarz@users.noreply.github.com>
2023-04-24 14:57:43 +0200

    drama analysis corpus es

--- R/notes_dd23.R
+++ R/notes_dd23.R
@@ -22,3 +22,38 @@ plot(
   vertex.label.font = 2L,
   vertex.frame.color = "white",
 )
+#### 13173.
+library(DramaAnalysis)
+#lm<-function(x) paste0(x,"drama")
+qdmeta<-lapply(qd,lm)
+lapply(qd,lm)
+loadMeta(list.files("data/qd/tg/csv"))
+loadMeta(qddf$X3[1])
+setDataDirectory(dataDirectory = "data/corpus")
+qdm<-matrix(qdmeta)
+library(xml2)
+attr(qdmeta[[1]],"language")
+qdmeta[[1]]
+f<-loadDrama("test:rjmw.0")
+installData("tg") #test, qd
+qddf<-loadAllInstalledIds(asDataFrame = T)
+lm<-function(x) loadMeta(x)
+dmeta<-loadMeta(qddf$X3)
+nr<-511
+dpres<-function(nr){
+#  f<-loadDrama(paste0(dmeta$corpus[nr],":",dmeta$drama[nr]))
+  m<-loadMeta(paste0(qddf$X1[nr],":",qddf$X3[nr]))
+  f<-loadDrama(paste0(qddf$X1[nr],":",qddf$X3[nr]))
+  print(m[,c(1:5,9)])
+  print(presence(f))
+  return(f)
+}
+#presence("test:rjmw.0")
+#m<-grep("test",dmeta$corpus)
+d1<-dpres(1)
+nr<-1
+#report(paste0(qddf$X1[nr],":",qddf$X3[nr]))
+ff<-frequencytable(d1)
+ff<-t(ff)
+fdf<-data.frame(token=rownames(ff),count=ff[,1])
+ff2<-fdf[order(ff[,],decreasing = T),]
