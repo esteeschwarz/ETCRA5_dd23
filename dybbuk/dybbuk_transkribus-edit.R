5b08d5938f07afb9157d6bf94b3f493756836080
estee <91368979+esteeschwarz@users.noreply.github.com>
2024-05-11 15:46:18 +0200

    1st replacement essai reimport to transkribus wks.

--- dybbuk/dybbuk_transkribus-edit.R
+++ dybbuk/dybbuk_transkribus-edit.R
@@ -42,5 +42,37 @@ tok.sort[m,]
 ### goal: to replace not vocalised tokens in xml with manually corrected tokens. pages 1-16 are not vocalise corrected,
 ### pages 17-23 are corrected.
 ##############################
+### manually assign corrected versions in .csv
+tok.ed<-read.csv("~/Documents/GitHub/ETCRA5_dd23/dybbuk/yudale_tok_freq/tok.freq.list.edited-yudale_tok_freq.csv")
+library(stringi)
+library(utils)
+fns.edit<-"~/boxHKW/21S/DH/local/EXC2020/dybbuk/Yudale_der_blinder,_Emkroyt1908-xpedit001/Yudale_der_blinder,_Emkroyt1908-xp001/page/"
+#sort regexes after gefräszigkeit:
+tok.ed.s<-tok.ed[order(tok.ed$bytes,decreasing = T),]
+#tok.ed.repl<-tok.ed$cor.tok[order(tok.ed$cor.tok)]
+### > TODO: replace order, first lagest 
+tok.ed$bytes<-unlist(lapply(tok.ed$WORD,object.size))
+#format(object.size("a"),units="B",digits=3,standard = "SI")
+#sort1<-stri_count(tok.ed$WORD,regex = ".")
+sort2<-rank(sort1,ties.method = "average")
+#sort1[sort2]
+for (k in 1:length(flist)){
+  file<-paste0(fns.edit,flist[k])
+  t1<-readLines(file)
+  
+  for (r in 1:length(tok.ed.s$WORD)){
+    reg<-tok.ed.s$WORD[r]
+    repl<-tok.ed.s$cor.tok[r]
+    if(!is.na(repl)&repl!="")
+       t1<-gsub(reg,repl,t1)
+    cat(k,":",r,"\n")
+    
+    
+  }
+  writeLines(t1,file)
+  
+}
+file
+
 
 
