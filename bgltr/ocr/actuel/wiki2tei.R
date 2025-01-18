1df29a4aa13505c500ac7b1d324b5d342580f535
estee <91368979+esteeschwarz@users.noreply.github.com>
2025-01-18 16:44:49 +0100

    wks., TODO: single <pb>

--- bgltr/ocr/actuel/wiki2tei.R
+++ bgltr/ocr/actuel/wiki2tei.R
@@ -14,6 +14,7 @@ htm<-read_html(t1)
 xml_text(htm)
 # no.
 #############
+get.all.elements<-function(){
 r2<-GET("https://de.wikisource.org/wiki/Franziska_Montenegro")
 t1<-content(r2,"text")
 htm<-read_html(t1)
@@ -34,20 +35,34 @@ m1<-grep("170%",all.h)
 m2<-grep("130%",all.h)
 xml_text(all.elements[m1])
 xml_text(all.elements[m2])
+return(all.elements)
+}
 # wks.
 #all.b<-xml_find_all(all.elements,"//b")
 #xml_text(all.b[1:20])
-x<-all.elements
-i<-41
-xi<-x[[90]]
-xi
-sub<-xi
-xml_text(xi)
+all.elements<-get.all.elements()
+# x<-all.elements
+# i<-41
+# xi<-x[[552]]
+# xi
+# sub<-xi
+# xml_text(xi)
 assign.sp<-function(x,i){
   sub<-x
   sub
 #  p<-xml_find_all(sub,"p")
   xml_name(xi)
+  m1<-grepl("170%",xml_attr(sub,"style"))
+  if(m1){
+    h2.tx<-paste0("#",xml_text(sub))
+    return(h2.tx)
+  }  
+  m2<-grepl("130%",xml_attr(sub,"style"))
+  if(m2){
+    h3.tx<-paste0("##",xml_text(sub))
+    return(h3.tx)
+  }
+  
   pb<-xml_attr(sub,"class")=="PageNumber"
     if(!is.na(pb)){
     pb<-xml_text(sub)
@@ -64,7 +79,7 @@ assign.sp<-function(x,i){
     #print(b.tx)
 #    p.tx<-gsub(b.tx,"",p.tx)
  #   b.tx<-gsub("\\.",":",b.tx)
-    sp.ezd<-paste0("$ ",p.tx)
+    sp.ezd<-paste0("$",p.tx)
     sp.ezd<-gsub("\n"," ",sp.ezd)
 #    sp.ezd<-gsub(":[ ]{1,2}\t",":\t",sp.ezd)
     sp.ezd<-gsub("\\[([0-9]{1,100})\\]",'<pb n="\\1"/>',sp.ezd)
@@ -79,7 +94,7 @@ assign.sp<-function(x,i){
     print(b.tx)
 #    p.tx<-gsub(b.tx,"",p.tx)
  #   b.tx<-gsub("\\.",":",b.tx)
-    sp.ezd<-paste0("$ ",p.tx)
+    sp.ezd<-paste0("$",p.tx)
     sp.ezd<-gsub("\n"," ",sp.ezd)
     #sp.ezd<-gsub(":[ ]{1,2}\t",":\t",sp.ezd)
     sp.ezd<-gsub("\\[([0-9]{1,100})\\]",'<pb n="\\1"/>',sp.ezd)
@@ -100,17 +115,8 @@ assign.sp<-function(x,i){
   sp.ezd<-gsub("\\[([0-9]{1,100})\\]",'<pb n="\\1"/>',sp.ezd)
   return(sp.ezd)
   }
-  div<-xml_find_all(sub,"div")
-  m1<-grepl("170%",xml_attr(sub,"style"))
-  if(m1){
-    h2.tx<-paste0("#",xml_text(sub))
-    return(h2.tx)
-  }  
-  m2<-grepl("130%",xml_attr(sub,"style"))
-  if(m2){
-    h3.tx<-paste0("##",xml_text(sub))
-    return(h3.tx)
-  }
+#  div<-xml_find_all(sub,"div")
+ 
 }
 # ###
 # result <- lapply(seq_along(my_list), function(i) {
@@ -120,9 +126,28 @@ assign.sp<-function(x,i){
 sp.lines<-lapply(seq_along(all.elements),function(i){
   assign.sp(all.elements[[i]],i)
 })
+ezd.lines<-unlist(sp.lines)
+#ezd.lines[310:330]
+m<-is.null(ezd.lines[1:length(ezd.lines)])
+sp.lines[[322]]
+s1<-sp.lines[[322]]
+s1
+m<-ezd.lines=="$"|ezd.lines=="@ *"|ezd.lines==""|
+  ezd.lines=="[ ]{1,10}"|ezd.lines=="[.]{1,2}"|ezd.lines=="\n"
+sum(m)
+ezd.lines<-ezd.lines[!m]
+ezd.lines<-gsub("\n","",ezd.lines)
+ezd.lines[632]
+### wks.
+m<-grepl("<pb",ezd.lines)
+sum(m)
+ezd.lines[m]
+utf8ToInt("
+          ")
 head(sp.lines,200)
-xml_text(x[[88]])
-sp.lines[[88]]
+xml_text(x[[8]])
+sp.lines[[8]]
 head(xml_text(x),30)
-x[[79]]
-m<-is.null(unlist(sp.lines))
+x[[9]]
+#m<-isunlist(sp.lines)
+writeLines(unlist(ezd.lines),"~/Documents/GitHub/ETCRA5_dd23/bgltr/ocr/actuel/ezd/steltzer_ezd.001.txt")
